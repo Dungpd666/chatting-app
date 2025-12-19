7e6ae8460479b79f86924c602d32352b698b69db
dungpd666 <pducdung.it@gmail.com>
2025-12-19 15:41:12 +0700

    feat: update app module for new features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConversationsModule } from './conversations/conversations.module';
+import { ConversationMembersModule } from './conversation_members/conversation_members.module';
+import { MessagesModule } from './messages/messages.module';
+import { AuthModule } from './auth/auth.module';
+import { User } from "./users/entities/user.entity";
+import { Conversation } from "./conversations/entities/conversation.entity";
+import { ConversationMember } from "./conversation_members/entities/conversation_member.entity";
+import { Message } from "./messages/entities/message.entity";
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: parseInt(process.env.DB_PORT || '5432', 10),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            entities: [User, Conversation, ConversationMember, Message],
+            synchronize: false
+        }),
+        UsersModule,
+        ConversationsModule,
+        ConversationMembersModule,
+        MessagesModule,
+        AuthModule,
+    ],  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
